717a6f30455e783a24408caeebc4bd7081d60859
Md.Mainul Hasan <37740006+maainul@users.noreply.github.com>
2022-07-22 11:59:18 +0600

    Update get_in_touch.go

--- api/storage/postgres/get_in_touch.go
+++ api/storage/postgres/get_in_touch.go
@@ -12,17 +12,21 @@ import (
 const crtgit = `
 INSERT INTO get_in_touch (
 	name,
+	email,
+	message,
 	created_at,
 	created_by
 ) VALUES (
 	:name,
+	:email,
+	:message,
 	:created_at,
 	:created_by
 ) RETURNING
 	id
 `
 
-func (s *Storage) CreateGetInTouch(ctx context.Context, blg storage.GetInTouch) (string, error) {
+func (s *Storage) CreateGetInTouch(ctx context.Context, git storage.GetInTouch) (string, error) {
 	log := logging.FromContext(ctx)
 	stmt, err := s.db.PrepareNamed(crtgit)
 	if err != nil {
@@ -31,7 +35,7 @@ func (s *Storage) CreateGetInTouch(ctx context.Context, blg storage.GetInTouch) 
 	}
 	defer stmt.Close()
 	var id string
-	if err := stmt.Get(&id, blg); err != nil {
+	if err := stmt.Get(&id, git); err != nil {
 		logging.WithError(err, log).Error("Failed to insert GetInTouch")
 		return "", status.Errorf(codes.Internal, "%d", err)
 
