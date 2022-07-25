8fc269c5ee06f765a8c2287c0b2cd956061c8e66
Md.Mainul Hasan <37740006+maainul@users.noreply.github.com>
2022-07-25 14:06:12 +0600

    Update postgres_test.go

--- api/storage/postgres/postgres_test.go
+++ api/storage/postgres/postgres_test.go
@@ -5,11 +5,19 @@ import (
 	"os"
 	"path/filepath"
 	"testing"
+	"fmt"
 )
 
 func TestMain(m *testing.M) {
 	const dbConnEnv = "DATABASE_CONNECTION"
 	ddlConnStr := os.Getenv(dbConnEnv)
+	fmt.Println("###################################")
+	fmt.Println("###################################")
+	fmt.Println("###################################")
+	fmt.Println(ddlConnStr)
+	fmt.Println("###################################")
+	fmt.Println("###################################")
+	fmt.Println("###################################")
 	if ddlConnStr == "" {
 		log.Printf("%s is not set, skipping", dbConnEnv)
 		return
